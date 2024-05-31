165a8a7bd23b2ec06248725d533ed44000e92e42
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-05-31 15:42:08 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,141 @@
+// 1-masala
+// let a= +prompt("Kvadratlarning tomonini kiriting");
+// let P= 4 * a;
+// console.log(P);
+
+// 2-masala
+// let a=+prompt("Kvadratlarning tomonini kiriting");
+// let S= a *a ;
+// console.log(S);
+
+// 3-masala
+// let a= +prompt("Kvadratlarning tomonini kiriting");
+// let b= +prompt("Kvadratlarning tomonini kiriting");
+// let S= a * b;
+// let P= 2 * (a + b);
+// console.log(S);
+// console.log(P);
+
+// 4-masala
+// let d= +prompt("Aylanani diametrini kiriting");
+// let L= Math.PI * d
+// console.log(L);
+
+// 5-masala
+// let a= +prompt("Kubning tomonini kiriting");
+// let V= a * a * a;
+// let S= 6 * a * a;
+// console.log(V);
+// console.log(S);
+
+// 6-masala
+// let a= +prompt("Paralelepetning yon tomonini kiriting");
+// let b= +prompt("Paralelepetning yon tomonini kiriting");
+// let c= +prompt("Paralelepetning yon tomonini kiriting");
+// let V= a * b * c;
+// let S= 2 * (a * b + b * c + a * c);
+// console.log(V);
+// console.log(S);
+
+// 7-masala
+// let R= +prompt("Doiraning radiusini kiriting");
+// let L= 2 *Math.PI *R;
+// let S= Math.PI * R*R;
+// console.log(L);
+// console.log(S);
+
+// 8-masala
+// let a= +prompt("Ixtiyori son kiriting");
+// let A= (a+b)/2;
+// console.log(A);
+
+// 9-masala
+// let R1= +prompt("Aylana radiusini kiriting");
+// let R2= +prompt("Aylana radiusini kiriting");
+// let S3= Math.PI * (R1-R2);
+// console.log(S3);
+
+// 10-masala
+// let 
+
+// 11-masala
+// let L= +prompt("uzunlik sm berilgan");
+// let l= L/100;
+// console.log(l);
+
+// 12-masala
+// let M= +prompt("ogirlik kg berilgan");
+// let m= M/1000;
+// console.log(m);
+
+// 13-masala
+// let b= +prompt("fayl xajmi b berilgan");
+// let kb= b/1024;
+// console.log(kb);
+
+// 14-masala va 15-masala
+// let A= +prompt("ixtiyori son");
+// let B= +prompt("ixtiyori son");
+// let whole= A / B;
+// let residul= A % B;
+// console.log(whole);
+// console.log(residul);
+
+// 16-masala
+// let num;
+// let O= num / 10;
+// let B= num % 10;
+// console.log(O);
+// console.log(B);
+
+// 17-masala
+// let num;
+// let O= num / 10;
+// let B= num % 10;
+// let s= O + B;
+// console.log(s);
+
+// 18-masala
+// let num;
+// let O= num / 10;
+// let B= num % 10;
+// let t= B * 10+ O;
+// console.log(t);
+
+// 19-masala
+// let num;
+// let Y= num / 100;
+// console.log(Y);
+
+// 20-masala
+// let num;
+// let B= num % 100;
+// let O= num / 10 % 10;
+// console.log(B);
+// console.log(O);
+
+// 21-masala
+// let num;
+// let B= num % 100;
+// let O= num / 10 % 10;
+// let Y= num / 100;
+// let s= B + O + Y;
+// console.log(s);
+
+// 22-masala
+// let num;
+// let num;
+// let B= num % 100;
+// let O= num / 10 % 10;
+// let Y= num / 100;
+// let t= B * 100 + O * 10 + Y;
+// console.log(t);
+
+// 23-masala
+// let num;
+// let num;
+// let B= num % 100;
+// let O= num / 10 % 10;
+// let Y= num / 100;
+// let b= B * 100 + Y * 10 + O;
+// console.log(b);
